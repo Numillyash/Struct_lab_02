38705cb641dc2f43f1a819ef14ed3394b28401b3
Numillyash <Numillyash-std@yandex.ru>
2022-09-29 00:39:54 +0300

    idk

--- Struct_lab_02/LCG.c
+++ Struct_lab_02/LCG.c
@@ -1,142 +1,142 @@
 #include "output.h"
 
 ull mul_mod(ull a, ull b, ull m) {
-    ull d = 0, _a=a, _b=b;
-    int i_a =0 , i_b = 0, zn = 1;
-    
-    while (_a) // подсчет битов a
-    {
-        i_a++;
-        _a >>= 1;
-    }
-    while (_b) // подсчет битов b
-    {
-        i_b++;
-        _b >>= 1;
-    }
-    
-    if(i_a + i_b <= 64) // если сумма битов не превышает 64
-        return a * b % m; // возвращаем обычным действиями
-    
-    // идея: a mod m = (a-m) mod m
-    // так как мы храним только неотрицательные числа, 
-    // будем хранить m-a и если что менять знак
-    // (-a) mod m = -(a mod m) = m - (a mod m) = m - a (a < m)
-    //
-    // (-a * b) mod m = (-a) mod m * b mod m = -(a mod m * b mod m) =
-    // = -((a * b) mod m) = m - ((a * b) mod m)
-    // 
-    // ((a * b) mod m) = m - (-a * b) mod m
-    //
-    // (-a * -b) mod m = (-a) mod m * (-b) mod m = 
-    // = (a mod m * b mod m) = (a * b) mod m
-    // 
-    // (a * b) mod m = (-a * -b) mod m
+	ull d = 0, _a = a, _b = b;
+	int i_a = 0, i_b = 0, zn = 1;
 
-    if(i_a > 32) 
-    {
-        _a = m - a;
-        zn *= -1;
-    }
-    else
-        _a = a;
-    if(i_b > 32)
-    {
-        _b = m - b;
-        zn *= -1;
-    }
-    else
-        _b = b;    
+	while (_a) // подсчет битов a
+	{
+		i_a++;
+		_a >>= 1;
+	}
+	while (_b) // подсчет битов b
+	{
+		i_b++;
+		_b >>= 1;
+	}
 
-    // считаем то, что гарантированно влезло в 64 бита
-    d = _a * _b;
+	if (i_a + i_b <= 64) // если сумма битов не превышает 64
+		return a * b % m; // возвращаем обычным действиями
 
-    d %= m;
+	// идея: a mod m = (a-m) mod m
+	// так как мы храним только неотрицательные числа, 
+	// будем хранить m-a и если что менять знак
+	// (-a) mod m = -(a mod m) = m - (a mod m) = m - a (a < m)
+	//
+	// (-a * b) mod m = (-a) mod m * b mod m = -(a mod m * b mod m) =
+	// = -((a * b) mod m) = m - ((a * b) mod m)
+	// 
+	// ((a * b) mod m) = m - (-a * b) mod m
+	//
+	// (-a * -b) mod m = (-a) mod m * (-b) mod m = 
+	// = (a mod m * b mod m) = (a * b) mod m
+	// 
+	// (a * b) mod m = (-a * -b) mod m
 
-    if(zn == -1) // если числа менялись
-        d = m-d;
+	if (i_a > 32)
+	{
+		_a = m - a;
+		zn *= -1;
+	}
+	else
+		_a = a;
+	if (i_b > 32)
+	{
+		_b = m - b;
+		zn *= -1;
+	}
+	else
+		_b = b;
 
-    return d;
+	// считаем то, что гарантированно влезло в 64 бита
+	d = _a * _b;
+
+	d %= m;
+
+	if (zn == -1) // если числа менялись
+		d = m - d;
+
+	return d;
 }
 
 ull sum_mod(ull a, ull b, ull m) {
-    ull d = 0, _a=a, _b=b;
-    int i_a =0 , i_b = 0, zn = 0;
-    
-    while (_a) // подсчет битов a
-    {
-        i_a++;
-        _a >>= 1;
-    }
-    while (_b) // подсчет битов b
-    {
-        i_b++;
-        _b >>= 1;
-    }
+	ull d = 0, _a = a, _b = b;
+	int i_a = 0, i_b = 0, zn = 0;
 
-    if((max(i_a, i_b)) <= 63) // если сумма битов не превышает 64
-        return (a + b) % m; // возвращаем обычным действиями
+	while (_a) // подсчет битов a
+	{
+		i_a++;
+		_a >>= 1;
+	}
+	while (_b) // подсчет битов b
+	{
+		i_b++;
+		_b >>= 1;
+	}
 
-    // идея: a mod m = (a-m) mod m
-    // так как мы храним только неотрицательные числа, 
-    // будем хранить m-a и если что менять знак
-    // (-a) mod m = -(a mod m) = m - (a mod m) = m - a (a < m)
-    //
-    // если заменены оба числа
-    // (a+b) mod m = m - (_a + _b) = m - (m - a + m - b) = (a + b - m) = (a+b) mod m
-    //
-    // если заменено a
-    // (a+b) mod m = - _a + b = -(m - a) + b = a + b - m = (a+b) mod m
-    // если _a > b (b - _a < 0)
-    // - _a + b = -(_a - b) = m - (_a - b)
-    //
-    // если заменено b
-    // (a+b) mod m = a - _b = a - (m - b) = a + b - m = (a+b) mod m
-    // если _b > a (a - _b < 0)
-    // a - _b = -(_b - a) = m - (_b - a)
-    //
+	if ((max(i_a, i_b)) <= 63) // если сумма битов не превышает 64
+		return (a + b) % m; // возвращаем обычным действиями
 
-    if(i_a > 63) 
-    {
-        _a = m - a;
-        zn = 1;
-    }
-    else
-        _a = a;
-    if(i_b > 63)
-    {
-        _b = m - b;
-        if(zn == 1)
-            zn = 3;
-        else 
-            zn = 2;
-    }
-    else
-        _b = b;    
+	// идея: a mod m = (a-m) mod m
+	// так как мы храним только неотрицательные числа, 
+	// будем хранить m-a и если что менять знак
+	// (-a) mod m = -(a mod m) = m - (a mod m) = m - a (a < m)
+	//
+	// если заменены оба числа
+	// (a+b) mod m = m - (_a + _b) = m - (m - a + m - b) = (a + b - m) = (a+b) mod m
+	//
+	// если заменено a
+	// (a+b) mod m = - _a + b = -(m - a) + b = a + b - m = (a+b) mod m
+	// если _a > b (b - _a < 0)
+	// - _a + b = -(_a - b) = m - (_a - b)
+	//
+	// если заменено b
+	// (a+b) mod m = a - _b = a - (m - b) = a + b - m = (a+b) mod m
+	// если _b > a (a - _b < 0)
+	// a - _b = -(_b - a) = m - (_b - a)
+	//
 
-    if(zn == 3)
-    {
-        d = m-(_a + _b);
-    }
-    if(zn == 2)
-    {
-        if(a - _b < 0)
-            d = m - (_b - a);
-        else
-            d = a - _b;
-    }
-    if(zn == 1)
-    {
-        if(b - _a < 0)
-            d = m - (_a - b);
-        else
-            d = b - _a;
-    }
-    if(zn == 0)
-    {
-        d = _a + _b;
-    }
-    return d;
+	if (i_a > 63)
+	{
+		_a = m - a;
+		zn = 1;
+	}
+	else
+		_a = a;
+	if (i_b > 63)
+	{
+		_b = m - b;
+		if (zn == 1)
+			zn = 3;
+		else
+			zn = 2;
+	}
+	else
+		_b = b;
+
+	if (zn == 3)
+	{
+		d = m - (_a + _b);
+	}
+	if (zn == 2)
+	{
+		if (a - _b < 0)
+			d = m - (_b - a);
+		else
+			d = a - _b;
+	}
+	if (zn == 1)
+	{
+		if (b - _a < 0)
+			d = m - (_a - b);
+		else
+			d = b - _a;
+	}
+	if (zn == 0)
+	{
+		d = _a + _b;
+	}
+	return d;
 }
 
 struct _system {
@@ -170,7 +170,7 @@ ull rand_get_next()
 {
 	generator.x0 = generator.next;
 	generator.next = sum_mod(mul_mod(generator.a, generator.x0, generator.m), generator.c, generator.m);
-    // = (a*x0+c)%m;
+	// = (a*x0+c)%m;
 	return generator.next;
 }
 
@@ -183,7 +183,7 @@ void rand_init(ull _a, ull _x0, ull _c, ull _m)
 	generator.next = _x0;
 }
 
-void get_c_wm(ull numbers[6]) 
+void get_c_wm(ull numbers[6])
 {
 	ull c, m = numbers[3], c_max = numbers[2], c_min = numbers[1];
 	ull count = 0;
@@ -210,11 +210,11 @@ void get_c_wm(ull numbers[6])
 	}
 }
 
-void get_a_wm(ull numbers[6]) 
+void get_a_wm(ull numbers[6])
 {
 	ull b = 1, m, a;
 	ull i = 3;
-	
+
 	m = numbers[1];
 
 	if (m % 2 == 0)
@@ -243,7 +243,7 @@ void get_a_wm(ull numbers[6])
 	a = b + 1;
 	m = numbers[1];
 	printf("%d %d\n", a, m);
-	if(a > m || a == 0 || m == 0) 
+	if (a > m || a == 0 || m == 0)
 	{
 		save_text("no solution");
 	}
@@ -271,32 +271,109 @@ void lcg_wm(ull numbers[6])
 	}
 }
 
+ull charToUll(char* str)
+{
+	ull res = 0, step = 1;
+
+	for (int i = strlen(str) - 1; i >= 0; i--)
+	{
+		res += str[i] == '1' ? step : 0;
+		step <<= 1;
+	}
+
+	return res;
+}
+
+ull getMean(char* inp_file)
+{
+	FILE* inp = fopen(inp_file, "r");
+	if (inp == NULL)
+	{
+		save_text("no solution");
+		exit(EXIT_SUCCESS);
+	}
+
+	ull count = 0, mean, now;
+	char* bin_num[70];
+
+	while (fgets(bin_num, 66, inp) != NULL)
+	{
+		char* backN = strchr(bin_num, '\n');
+		if (backN != NULL)
+			*backN = '\0';
+
+		now = charToUll(bin_num);
+		count++;
+
+		mean = (ull)(((double)mean * (count - 1) + now) / count);
+	}
+
+	printf("\n");
+	fclose(inp);
+	return mean;
+}
+
+void getCounts(char* inp_file, ull* serCount, ull* serLength, ull* length, ull mean)
+{
+	FILE* inp = fopen(inp_file, "r");
+	if (inp == NULL)
+	{
+		save_text("no solution");
+		exit(EXIT_SUCCESS);
+	}
+
+	ull count = 0, prev = 255, now_ull, now, len = 0;
+	char* bin_num[70];
+
+	while (fgets(bin_num, 66, inp) != NULL)
+	{
+		char* backN = strchr(bin_num, '\n');
+		if (backN != NULL)
+			*backN = '\0';
+
+		now_ull = charToUll(bin_num);
+
+		now = now_ull > mean ? 1 : 0;
+
+		if (now != prev)
+		{
+			(*serCount)++;
+			if (len > *serLength)
+				*serLength = len;
+			len = 0;
+		}
+		else
+		{
+			len++;
+		}
+		prev = now;
+		count++;
+
+	}
+	*length = count;
+	printf("\n");
+	fclose(inp);
+}
+
 void test_wm(char* inp_file)
 {
-    ull count = 0;
-    FILE* inp = fopen(inp_file, "r");
-    if (inp == NULL)
-    {
-        save_text("no solution");
-        exit(EXIT_SUCCESS);
-    }
-    
-    // 64 бита
-    //
-    // то бишь можно разделить на 8 блоков по 8 бит и считать кол-ва единичек и нулей
-    //
 
-    char* bin_num[70];
-    fscanf(inp, "%s", bin_num);
+	ull serCount = 0, lenCount = 0, count, mean;
 
-    while (strlen(bin_num) > 0)
-    {
-        if (strlen(bin_num) > 64)
-        {
-            save_text("no solution");
-            exit(EXIT_SUCCESS);
-        }
+	mean = getMean(inp_file);
+	getCounts(inp_file, &serCount, &lenCount, &count, mean);
 
-    }
+	char b1, b2;
+	double percent[6] = { 0.10,0.05,0.025,0.02,0.01,0.005 };
+	double koef[6] = { 1.645,1.96,2.241,2.326,2.576,2.807 };
 
+#define NUMB 1
+
+	b1 = lenCount < (ull)(3.3 * (log((double)count) + 1)) ? 1 : 0;
+	b2 = serCount > (ull)(0.5 * (count + 1 - koef[NUMB] * sqrt((double)count - 1))) ? 1 : 0;
+
+	if (b1 & b2)
+		printf("for a = %f and k = %f alls good", percent[NUMB], koef[NUMB]);
+	else
+		printf("for a = %f and k = %f alls bad", percent[NUMB], koef[NUMB]);
 }
